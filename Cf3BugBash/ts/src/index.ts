2d18db46c5028f2b121656a78f702ae4a666133f
hugovar93 <89418097+hugovar93@users.noreply.github.com>
2022-12-28 14:14:49 -0600

    folder added 2

---
+++ Cf3BugBash/ts/src/index.ts
@@ -0,0 +1,13 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+ export const scenario2ts = functions.https.onRequest((request, response) => {
+   functions.logger.info("Hello logs!", {structuredData: true});
+   response.send("Hello from Firebase!");
+});
+export const scenario3ts = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
